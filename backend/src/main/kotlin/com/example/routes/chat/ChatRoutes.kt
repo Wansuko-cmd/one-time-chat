c1fd4757c32a2c2732238d70b7a6541ddaffb812
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-04-19 23:55:18 +0900

    [add] Hashの作成機能の追加

--- backend/src/main/kotlin/com/example/routes/chat/ChatRoutes.kt
+++ backend/src/main/kotlin/com/example/routes/chat/ChatRoutes.kt
@@ -1,16 +1,39 @@
 package com.example.routes.chat
 
+import com.example.db.entities.Room
+import com.example.db.tables.Rooms
 import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.transactions.transaction
 import java.util.*
+import kotlin.properties.Delegates
 
 fun Route.chatRoutes(){
     get("/chat") {
+        var newHash = UUID.randomUUID().toString()
+        var isExist = false
 
-        val hash = UUID.randomUUID().toString()
-        call.respondRedirect("/chat/$hash")
+        //ユニークなチャット部屋ハッシュであることを検証
+        while (true){
+            transaction {
+                isExist = Room.find { Rooms.hash eq newHash }.count() <= 0
+            }
+            if(isExist){
+                break
+            }
+            newHash = UUID.randomUUID().toString()
+        }
+
+        transaction {
+            Rooms.insert {
+                it[hash] = newHash
+            }
+        }
+
+        call.respondRedirect("/chat/$newHash")
     }
 
     get("/chat/{hash}") {
